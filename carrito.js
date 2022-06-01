7e8b555589400018047a1ad70ae77278c24e7c5f
Bogarin1608 <105824676+Bogarin1608@users.noreply.github.com>
2022-06-01 15:36:47 -0600

    Add files via upload

---
+++ carrito.js
@@ -0,0 +1,50 @@
+﻿//comprar productos
+class Carrito{
+    comprarProducto(e){
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')){
+            const producto =e. target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+    leerDatosProducto(producto){
+        const infoproducto ={
+            imagen : producto.querySelector('img').src,
+            titulo : producto.querySelector('h4').textContent,
+            precio : producto.querySelector('.precio span').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad : 1
+        }
+        this.insertCarrito(infoproducto);
+
+    }
+    insertCarrito(producto){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+         <img src="${producto.imagen}" width=100>
+        </td>
+        <td>
+        ${producto.titulo}
+        </td>
+        <td>
+        ${producto.precio}
+        </td>
+        <td>
+         <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+        </td>
+        `;
+        listaProductos.appendChild(row);
+        
+    }
+    eliminarProducto(e){
+        e.preventDefault();
+        let producto, productoID;
+        if(e.target.classList.contains('borrar-producto')){
+            e.target.parentElement.parentElement.remove();
+            producto = e.target.parentElement.parentElement;
+            productoID =producto.querySelector('a').getAttribute('data-id');
+        }
+    }
+}
+
